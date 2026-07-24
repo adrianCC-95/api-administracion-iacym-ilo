184818a0cd2c0b4df42bd3798a77901575ed5999
adrianCC-95 <ahuanacuni@plaborum.com.pe>
2026-07-24 18:31:55 -0500

    feat: dashboard services add values graficos

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -12,34 +12,52 @@ export class DashboardService {
     async getDashboard() {
         const currentYear = new Date().getFullYear();
 
-        const [incomeSummary, incomeMonthly, memberRanking, totalMembers] = await Promise.all([
-            this.reportsService.incomeSummary({
-                startDate: `${currentYear}-01-01`,
-                endDate: `${currentYear}-12-31`,
-            }),
-            this.reportsService.incomeMonthly({
-                startDate: `${currentYear}-01-01`,
-                endDate: `${currentYear}-12-31`,
-            }),
-            this.reportsService.incomeByMemberPaginated({
-                page: 1,
-                size: 5,
-                startDate: `${currentYear}-01-01`,
-                endDate: `${currentYear}-12-31`,
-                sortField: 'total',
-                sortDirection: 'DESC',
-            }),
-            this.membersService.count(),
-        ]);
+        const [incomeSummary, expenseSummary, expenseMonthly, incomeMonthly, memberRanking, totalMembers] =
+            await Promise.all([
+                this.reportsService.incomeSummary({
+                    startDate: `${currentYear}-01-01`,
+                    endDate: `${currentYear}-12-31`,
+                }),
+
+                this.reportsService.expenseSummary({
+                    startDate: `${currentYear}-01-01`,
+                    endDate: `${currentYear}-12-31`,
+                }),
+
+                this.reportsService.expenseMonthly({
+                    startDate: `${currentYear}-01-01`,
+                    endDate: `${currentYear}-12-31`,
+                }),
+
+                this.reportsService.incomeMonthly({
+                    startDate: `${currentYear}-01-01`,
+                    endDate: `${currentYear}-12-31`,
+                }),
+                this.reportsService.incomeByMemberPaginated({
+                    page: 1,
+                    size: 5,
+                    startDate: `${currentYear}-01-01`,
+                    endDate: `${currentYear}-12-31`,
+                    sortField: 'total',
+                    sortDirection: 'DESC',
+                }),
+                this.membersService.count(),
+            ]);
 
         return {
             cards: {
                 incomes: incomeSummary.total,
-                expenses: 0,
+                expenses: expenseSummary.total,
                 members: totalMembers,
             },
             incomeMonthly,
+            expenseMonthly,
             memberRanking,
+            stats: {
+                ministries: 10,
+                users: 20,
+                members: totalMembers,
+            },
         };
     }
 }
